c794066082de122513e324723acc45e14b9ac3fb
Benimana Jean Damascene <benidama02@gmail.com>
2025-07-12 10:10:07 -0700

    all git conflicts solved

--- buildinJs/challange1.js
+++ buildinJs/challange1.js
@@ -2,3 +2,4 @@ const numbers = [2,5,8,10,3];
 const result =  numbers.map(num => num*2).filter(num => num < 10).sort((a,b) => b-a);
 console.log(result)
 
+
--- distruction/opar.js
+++
@@ -1,4 +0,0 @@
-const ath = require("./math")
-
-console.log("addition: "+ ath.add(2,67))
-console.log("substruction: "+ ath.substruction(2,67))
